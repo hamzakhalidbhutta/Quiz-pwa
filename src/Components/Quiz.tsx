02bc17bbb7095aa4c317d9efba48a8991ecd63aa
Hamza Khalid Bhutta <hamzakhalidbhutta430@gmail.com>
2022-02-22 12:21:40 +0500

    Updated Quiz to show single Question at a time

--- src/Components/Quiz.tsx
+++ src/Components/Quiz.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { TimerContext } from "../store/Context/TimerContext";
 
 const Quiz = ({ data }: any) => {
@@ -8,15 +8,24 @@ const Quiz = ({ data }: any) => {
     checkTimer,
   }: any = useContext(TimerContext);
   useEffect(() => {
-    setInterval(() => {
-      checkTimer();
-    }, 1000);
+    setInterval(
+      () => {
+        checkTimer();
+      },
+      1000,
+      time.total_duration
+    );
   }, []);
   useEffect(() => {
-    if (time.counter > time.total_duration) {
+    if (
+      time.counter > time.total_duration ||
+      questionNumber.current >= data.length
+    ) {
       stopTimer();
     }
   });
+
+  let questionNumber = useRef(0);
   return (
     <section className="Quiz">
       <h5>
@@ -27,50 +36,64 @@ const Quiz = ({ data }: any) => {
       {data &&
         data.length &&
         data.map((v: any, i: any) => {
-          return (
-            <div key={i}>
-              <h2>
-                {v.quiz_question
-                  .replace(/&#039;/g, "'")
-                  .replace(/&quot;/g, '"')
-                  .replace(/&eacute;/g, "é")
-                  .replace(/&shy;/g, " ")}
-              </h2>
-              <button>
-                {v.answers[0][0]
-                  .replace(/&#039;/g, "'")
-                  .replace(/&quot;/g, '"')
-                  .replace(/&eacute;/g, "é")
-                  .replace(/&shy;/g, " ")}
-              </button>
-              <br /> <br />
-              <button>
-                {v.answers[0][1]
-                  .replace(/&#039;/g, "'")
-                  .replace(/&quot;/g, '"')
-                  .replace(/&eacute;/g, "é")
-                  .replace(/&shy;/g, " ")}
-              </button>
-              <br /> <br />
-              <button>
-                {v.answers[0][2]
-                  .replace(/&#039;/g, "'")
-                  .replace(/&quot;/g, '"')
-                  .replace(/&eacute;/g, "é")
-                  .replace(/&shy;/g, " ")}
-              </button>
-              <br /> <br />
-              <button>
-                {v.answers[0][3]
-                  .replace(/&#039;/g, "'")
-                  .replace(/&quot;/g, '"')
-                  .replace(/&eacute;/g, "é")
-                  .replace(/&shy;/g, " ")}
-              </button>
-              <br /> <br />
-            </div>
-          );
+          //   return (
+          if (i == questionNumber.current) {
+            return (
+              <div key={i}>
+                <h2>
+                  {v.quiz_question
+                    .replace(/&#039;/g, "'")
+                    .replace(/&quot;/g, '"')
+                    .replace(/&eacute;/g, "é")
+                    .replace(/&shy;/g, " ")}
+                </h2>
+                <button>
+                  {v.answers[0][0]
+                    .replace(/&#039;/g, "'")
+                    .replace(/&quot;/g, '"')
+                    .replace(/&eacute;/g, "é")
+                    .replace(/&shy;/g, " ")}
+                </button>
+                <br /> <br />
+                <button>
+                  {v.answers[0][1]
+                    .replace(/&#039;/g, "'")
+                    .replace(/&quot;/g, '"')
+                    .replace(/&eacute;/g, "é")
+                    .replace(/&shy;/g, " ")}
+                </button>
+                <br /> <br />
+                <button>
+                  {v.answers[0][2]
+                    .replace(/&#039;/g, "'")
+                    .replace(/&quot;/g, '"')
+                    .replace(/&eacute;/g, "é")
+                    .replace(/&shy;/g, " ")}
+                </button>
+                <br /> <br />
+                <button>
+                  {v.answers[0][3]
+                    .replace(/&#039;/g, "'")
+                    .replace(/&quot;/g, '"')
+                    .replace(/&eacute;/g, "é")
+                    .replace(/&shy;/g, " ")}
+                </button>
+                <br /> <br />
+              </div>
+            );
+          }
         })}
+
+      <div>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            questionNumber.current = questionNumber.current + 1;
+          }}
+        >
+          Next
+        </button>
+      </div>
     </section>
   );
 };
